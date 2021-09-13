0c82379929d133ad0fa536dcd41631f062c90270
rileymiller28 <rileymiller28@icloud.com>
2021-09-13 14:07:57 -0500

    Update Form1.js

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,1 +1,8 @@
 console.log(`This is a test for the tutorial demo.`);
+
+// put below the first line of code already there
+let myFirstName = prompt("Enter your first name: ")
+console.log(`Your first name is ${myFirstName}.`)
+let myLastName = prompt("Enter your last name: ")
+console.log(`Your first name is ${myLastName}.`)
+
